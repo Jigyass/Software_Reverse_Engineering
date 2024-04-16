67c8ba87e28cd9877fde41cb1555a648d4f5af9c
Jigyass <jigyassharma08@gmail.com>
2024-04-16 18:49:01 -0500

    Test failed Script not working

--- S24_700-Project1_Files/Script.c
+++ S24_700-Project1_Files/Script.c
@@ -1,68 +1,36 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <elf.h>
+/*
+@author: Jigyas Sharma
+@course: Software Reverse Engineering
+@description: This is a script that takes a file as an input and then checks if the file is a ELF binary, and if it is then prints more information about each section of the executable
+*/
 
-int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        fprintf(stderr, "Usage: %s <ELF-file>\n", argv[0]);
-        return EXIT_FAILURE;
-    }
+#Importing Libraries
+#include<iostream>
+#include<string>
 
-    FILE *file = fopen(argv[1], "rb");
-    if (!file) {
-        perror("Failed to open file");
-        return EXIT_FAILURE;
-    }
-
-    // Read the ELF header
-    Elf64_Ehdr header;
-    if (fread(&header, 1, sizeof(header), file) != sizeof(header)) {
-        fprintf(stderr, "Failed to read ELF header\n");
-        fclose(file);
-        return EXIT_FAILURE;
-    }
-
-    // Check for valid ELF magic number
-    if (memcmp(header.e_ident, ELFMAG, SELFMAG) != 0) {
-        fprintf(stderr, "This is not an ELF file.\n");
-        fclose(file);
-        return EXIT_FAILURE;
-    }
-
-    // Check for 64-bit ELF binary
-    if (header.e_ident[EI_CLASS] != ELFCLASS64) {
-        fprintf(stderr, "This file is not a 64-bit ELF binary.\n");
-        fclose(file);
-        return EXIT_FAILURE;
-    }
-
-    printf("ELF 64-bit Header:\n");
-    printf("  Machine: %u\n", header.e_machine);
-    printf("  Entry point address: 0x%lx\n", header.e_entry);
-
-    // Reading section headers
-    Elf64_Shdr *sec_headers = malloc(header.e_shentsize * header.e_shnum);
-    if (!sec_headers) {
-        perror("Failed to allocate memory for section headers");
-        fclose(file);
-        return EXIT_FAILURE;
-    }
-
-    fseek(file, header.e_shoff, SEEK_SET);
-    if (fread(sec_headers, header.e_shentsize, header.e_shnum, file) != header.e_shnum) {
-        fprintf(stderr, "Failed to read section headers\n");
-        free(sec_headers);
-        fclose(file);
-        return EXIT_FAILURE;
-    }
-
-    for (int i = 0; i < header.e_shnum; i++) {
-        printf("Section %d: offset 0x%lx, size 0x%lx\n", i, sec_headers[i].sh_offset, sec_headers[i].sh_size);
-    }
-
-    free(sec_headers);
-    fclose(file);
-    return EXIT_SUCCESS;
+/*
+@param: name of file or file path
+@ret: null
+@desc: This is the main function which takes in the file or file path and then calls different functions over the file to check the validity of the file and extract information about the file
+*/
+int main(int argc, char* argv[])
+{
+	if(argc != 2)
+	{
+		std::cerr << "Correct Script Command: " << argv[0] <<" <file_name>/<file_path> \n";
+		return 1;
+	}
+	std::string filePath = argv[1];	
+	int fileStatus = checkELFStatus(filePath);
+	
+	switch(fileStatus) 
+	{
+		case 0:
+			std::cout<<"File is a valid ELF binary.\n";
+			std::cout<<"Processing Sections of Binary.\n";
+		case -1:
+			std::cerr<<"Error: System call failed.\n"
+		case -2:
+			std::cerr<<"Error: File is not a valid ELF
+	}
 }
-
