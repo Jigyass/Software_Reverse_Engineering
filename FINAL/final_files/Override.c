d4bedf83927616c2a555338f91379992e8c2fc63
Jigyass <jigyas@icloud.com>
2024-05-06 09:59:57 -0500

    Finished Final

---
+++ FINAL/final_files/Override.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+double sqrt(double base) 
+{
+	return 80;
+	/*
+    static int pow_calls = 0;
+    pow_calls++;
+    if (pow_calls == 1)
+    {
+        return 1.0;
+    } 
+    else if (pow_calls == 2) 
+    {
+        return 64.0;
+    } 
+    else 
+    {
+        return 0.0;
+    }*/
+}
+
+size_t strlen(const char* str)
+{
+	return 0;
+}
